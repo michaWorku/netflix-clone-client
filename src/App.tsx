fc2536d90f199549a3ebd393daf57747f7ca2af4
michaworku <mikias27worku@gmail.com>
2022-03-15 15:35:31 +0300

    app css layout and page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React, { FC } from 'react'
+import "./app.scss";
+import { FC } from "react";
+import {Home, Register, Login, Watch} from './pages'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
 
 const App : FC = () => {
+  
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path = '/' element ={<Home/>}  />
+        <Route path = '/register' element ={<Register />}  />
+        <Route path = '/login' element ={<Login />}  />
+        <Route path = '/movies' element ={ <Home />}  />
+        <Route path = '/series' element ={ <Home />}  />
+        <Route path = '/watch' element ={ <Watch />}  /> 
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
