446121a9eb58bdc81f401a40f5310c7cbb55521d
michaworku <mikias27worku@gmail.com>
2022-03-15 19:25:08 +0300

    register page

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,9 +1,63 @@
 import { FC } from 'react'
+import { useRef } from "react";
+import { useState } from "react";
+import "./Register.scss";
+
+interface MutableRefObject<T> {
+  current: T;
+}
 
 const Register : FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailRef = useRef<null | HTMLInputElement>(null);
+  const passwordRef = useRef<null | HTMLInputElement>(null);
+
+  const handleStart = () => {
+    if(emailRef && emailRef.current)
+      setEmail(emailRef.current?.value);
+  };
+  const handleFinish = () => {
+    if(passwordRef && passwordRef.current)
+      setPassword(passwordRef.current?.value);
+  };
   return (
-    <div>Register</div>
-  )
+    <div className="register">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+          <button className="loginButton">Sign In</button>
+        </div>
+      </div>
+      <div className="container">
+        <h1>Unlimited movies, TV shows, and more.</h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        {!email ? (
+          <div className="input">
+            <input type="email" placeholder="email address" ref={emailRef} />
+            <button className="registerButton" onClick={handleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (
+          <form className="input">
+            <input type="password" placeholder="password" ref={passwordRef} />
+            <button className="registerButton" onClick={handleFinish}>
+              Start
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Register
